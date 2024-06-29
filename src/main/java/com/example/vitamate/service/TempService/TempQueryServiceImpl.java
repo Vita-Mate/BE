32554104b4d692a78aa4393481e046a1718cf9c7
u-genuine <u_genuine@naver.com>
2024-06-30 08:40:15 +0900

    API 응답 통일 & 에러 핸들링

---
+++ src/main/java/com/example/vitamate/service/TempService/TempQueryServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.vitamate.service.TempService;
+
+import com.example.vitamate.apiPayload.code.status.ErrorStatus;
+import com.example.vitamate.apiPayload.exception.handler.TempHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TempQueryServiceImpl implements TempQueryService{
+
+    @Override
+    public void CheckFlag(Integer flag){
+
+        if(flag == 1){
+            throw new TempHandler(ErrorStatus.TEMP_EXCEPTION);
+        }
+    }
+}
