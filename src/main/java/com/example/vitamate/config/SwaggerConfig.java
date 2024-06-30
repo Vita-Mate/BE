d1566a5937471d698afad6c0dcab991fa2c38e87
u-genuine <u_genuine@naver.com>
2024-06-30 13:44:15 +0900

    스웨거 설정

---
+++ src/main/java/com/example/vitamate/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.example.vitamate.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI VitaMateAPI(){
+        String jwtSchemeName = "JWT TOKEN";
+
+        // API 요청 헤더에 인증 정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+
+    }
+
+    private Info apiInfo(){
+        return new Info()
+                .title("VitaMate API")
+                .description("VitaMate API 명세서")
+                .version("1.0.0");
+    }
+}
