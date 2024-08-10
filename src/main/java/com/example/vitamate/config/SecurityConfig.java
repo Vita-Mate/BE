4fce56348daeb95218241e5cac8b774c6d903bf2
u-genuine <u_genuine@naver.com>
2024-08-10 12:56:42 +0900

    헬스 체크 시큐리티 요청 허가

--- src/main/java/com/example/vitamate/config/SecurityConfig.java
+++ src/main/java/com/example/vitamate/config/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig {
                                 "/members/sign-up",
                                 "/swagger-ui/**",
                                 "/swagger-resources/**",
-                                "/v3/api-docs/**").permitAll()
+                                "/v3/api-docs/**",
+                                "/health").permitAll()
                         // USER 권한이 있어야 요청할 수 있음
                         .requestMatchers("/members/test").hasRole("USER")
                         // 이 밖에 모든 요청에 대해서 인증 필요
