2b419aca44ec52c494d9ddd1c89661cd45e1ea5a
u-genuine <u_genuine@naver.com>
2024-10-14 15:18:08 +0900

    챌린지 기간 enum 수정

--- src/main/java/com/example/vitamate/domain/enums/ChallengeDuration.java
+++ src/main/java/com/example/vitamate/domain/enums/ChallengeDuration.java
@@ -2,9 +2,8 @@ package com.example.vitamate.domain.enums;
 
 public enum ChallengeDuration {
     ONE_WEEK,
-    TWO_WEEKS,
-    THREE_WEEKS,
     ONE_MONTH,
-    TWO_MONTHS,
-    THREE_MONTHS
+    THREE_MONTHS,
+    SIX_MONTHS,
+    ONE_YEAR
 }
