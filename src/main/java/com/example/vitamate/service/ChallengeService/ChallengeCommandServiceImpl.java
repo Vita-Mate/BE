33c2f3895fd1b343100f86295987a07f7b62e8cc
u-genuine <u_genuine@naver.com>
2024-10-14 13:52:43 +0900

    사용자 검증 메서드 추가

--- src/main/java/com/example/vitamate/service/ChallengeService/ChallengeCommandServiceImpl.java
+++ src/main/java/com/example/vitamate/service/ChallengeService/ChallengeCommandServiceImpl.java
@@ -3,6 +3,11 @@ package com.example.vitamate.service.ChallengeService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.vitamate.apiPayload.code.status.ErrorStatus;
+import com.example.vitamate.apiPayload.exception.handler.MemberHandler;
+import com.example.vitamate.domain.Member;
+import com.example.vitamate.domain.enums.ChallengeCategory;
+import com.example.vitamate.repository.MemberRepository;
 import com.example.vitamate.web.dto.ChallengeRequestDTO;
 import com.example.vitamate.web.dto.ChallengeResponseDTO;
 
@@ -12,10 +17,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChallengeCommandServiceImpl implements ChallengeCommandService{
 
+	private final MemberRepository memberRepository;
+
 	@Override
 	@Transactional
 	public ChallengeResponseDTO createChallenge(String email, ChallengeRequestDTO requestDTO){
+		Member member = validMember(email);
 
 		return null;
+	}
+
+	private Member validMember(String email){
+		Member member = memberRepository.findByEmail(email)
+			.orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+		return member;
 	}
 }
