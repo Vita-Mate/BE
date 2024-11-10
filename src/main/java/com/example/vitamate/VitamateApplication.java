ef90dbeab582f2dfafa42017e1ea9fd9f963d64b
u-genuine <u_genuine@naver.com>
2024-11-10 16:01:24 +0900

    [bugfix] 자정에 챌린지 상태 변경되도록 어노테이션 추가

--- src/main/java/com/example/vitamate/VitamateApplication.java
+++ src/main/java/com/example/vitamate/VitamateApplication.java
@@ -3,9 +3,11 @@ package com.example.vitamate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class VitamateApplication {
 
 	public static void main(String[] args) {
